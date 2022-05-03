8a01834d141e8e8ae694dc4c8277b2e790d221a8
Tedd <fedsavin@gmail.com>
2022-05-03 20:27:28 +0300

    add floating placeholder

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const select = document.querySelector.bind(document)
 const selectAll = document.querySelectorAll.bind(document)
 
 const input = select('input')
-
 const label = select('label')
-
 const actionButton = select('.action-button')
-const values = selectAll('li')
-
 const valuesContainer = select('.select-values')
 const itemsContainer = select('.list-container')
+
+const values = selectAll('li')
+
 
 const inputWidth = input.offsetWidth;
 const labelInitialStyles = label.style;
@@ -17,53 +16,50 @@ const labelInitialStyles = label.style;
 itemsContainer.style.width = inputWidth - 24 + 'px'
 
 
-const BackLabelPosition = () => {
+const backLabelPosition = () => {
     label.style.transform = 'translateY(-25%)';
     label.style.top = '15%';
 }
 
+const controlPlaceholder = (show) => {
+    show ? input.placeholder = 'placeholder' : input.placeholder = ''
+}
+controlPlaceholder()
 document.body.addEventListener('click', (e) => {
-            if (!(e.target.classList.contains('action-button') || e.target.classList.contains('input-select'))) {
-                    console.log(e.target.classList);
-                    valuesContainer.style = "display:none"
-                    if (!input.value) {
-                        label.style = labelInitialStyles
-                    }
-                }
-            })
-
-
-        const showSelectMenu = () => {
-            valuesContainer.style = 'display:block'
-            itemsContainer.width = inputWidth
-            BackLabelPosition()
+    if (!(e.target.classList.contains('action-button') || e.target.classList.contains('input-select'))) {
+        valuesContainer.style = "display:none"
+        if (!input.value) {
+            label.style = labelInitialStyles
         }
-
-        input.addEventListener('click', () => {
-            showSelectMenu()
-        })
+    }
+})
 
 
-        values.forEach(element => {
-            element.addEventListener('click', () => {
-                input.value = element.innerText
-                valuesContainer.style = "display:none"
-                actionButton.dataset.state = "selected"
-            })
-        });
+input.addEventListener('click', () => {
+    valuesContainer.style = 'display:block'
+    backLabelPosition()
+})
 
-        actionButton.addEventListener('click', () => {
-            if (actionButton.dataset.state === 'ready') {
-                console.log('is ready');
-                valuesContainer.style = 'display:block'
-                itemsContainer.width = inputWidth
-                input.value = '';
-                BackLabelPosition()
-                return
-            }
-            console.log('back');
-            input.value = '';
-            actionButton.dataset.state = "ready"
-            label.style = labelInitialStyles;
-            valuesContainer.style = 'display:none'
-        })
+
+values.forEach(element => {
+    element.addEventListener('click', () => {
+        input.value = element.innerText
+        valuesContainer.style = "display:none"
+        actionButton.dataset.state = "selected"
+    })
+});
+
+actionButton.addEventListener('click', () => {
+    if (actionButton.dataset.state === 'ready') {
+        valuesContainer.style = 'display:block'
+        input.value = '';
+        controlPlaceholder()
+        backLabelPosition()
+        return
+    }
+    input.value = '';
+    actionButton.dataset.state = "ready"
+    label.style = labelInitialStyles;
+    valuesContainer.style = 'display:none'
+    controlPlaceholder()
+})
