691a324d5531c5f885e6bc7f2c85c95959dabf86
Tedd <fedsavin@gmail.com>
2022-05-03 14:20:54 +0300

    add data attribute

--- index.js
+++ index.js
@@ -2,42 +2,54 @@ const select = document.querySelector.bind(document)
 const selectAll = document.querySelectorAll.bind(document)
 
 const input = select('input')
-const inputWidth = input.offsetWidth;
+
 const label = select('label')
-const labelInitialStyles = label.style;
+
 const actionButton = select('.action-button')
+const values = selectAll('li')
 
 const valuesContainer = select('.select-values')
 const itemsContainer = select('.list-container')
 
+const inputWidth = input.offsetWidth;
+const labelInitialStyles = label.style;
 
-itemsContainer.style.width = inputWidth - 23 + 'px'
+itemsContainer.style.width = inputWidth - 24 + 'px'
+
+
+const BackLabelPosition = () => {
+    label.style.transform = 'translateY(-25%)';
+    label.style.top = '15%';
+}
 
 document.body.addEventListener('click', (e) => {
-    if (!e.target.classList.contains('input-select'))
+    if (!e.target.classList.contains('input-select')) {
         valuesContainer.style = "display:none"
+        if (input.value) {
+            BackLabelPosition()
+        }
+    }
 })
 
 input.addEventListener('click', () => {
     valuesContainer.style = 'display:block'
     itemsContainer.width = inputWidth
-    label.style.transform = 'translateY(-25%)';
-    label.style.top = '15%';
+    BackLabelPosition()
 })
 
-const values = selectAll('li')
+
 
 
 values.forEach(element => {
     element.addEventListener('click', () => {
         input.value = element.innerText
         valuesContainer.style = "display:none"
-        actionButton.style.display="block"
+        actionButton.dataset.state="selected"
     })
 });
 
-actionButton.addEventListener('click',()=>{
-    input.value='';
-    actionButton.style.display="none"
-    label.style=labelInitialStyles;
+actionButton.addEventListener('click', () => {
+    input.value = '';
+    actionButton.dataset.state="ready"
+    label.style = labelInitialStyles;
 })
