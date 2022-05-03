4cf20057d0795a3a585099e0c978db7f70f7903e
Tedd <fedsavin@gmail.com>
2022-05-03 23:25:39 +0300

    rewrite to class

--- index.js
+++ index.js
@@ -1,65 +1,80 @@
-const select = document.querySelector.bind(document)
-const selectAll = document.querySelectorAll.bind(document)
+class customSelectWithFloatLabel {
+    constructor() {
+        this.select = document.querySelector.bind(document)
+        this.selectAll = document.querySelectorAll.bind(document)
+        this.input = this.select('input')
+        this.label = this.select('label')
+        this.actionButton = this.select('.action-button')
+        this.valuesContainer = this.select('.select-values')
+        this.itemsContainer = this.select('.list-container')
+        this.values = this.selectAll('li')
+        this.inputWidth = this.input.offsetWidth;
+        this.labelInitialStyles = this.label.style;
+        this.itemsContainer.style.width = this.inputWidth - 24 + 'px'
+    }
 
-const input = select('input')
-const label = select('label')
-const actionButton = select('.action-button')
-const valuesContainer = select('.select-values')
-const itemsContainer = select('.list-container')
+    backLabelPosition = () => {
+        this.label.style.transform = 'translateY(-25%)';
+        this.label.style.top = '15%';
+    }
 
-const values = selectAll('li')
+    controlPlaceholder = (show) => {
+        show ? this.input.placeholder = 'placeholder' : this.input.placeholder = ''
+    }
 
+    addClick() {
+        document.body.addEventListener('click', (e) => {
+            if (!(e.target.classList.contains('action-button') || e.target.classList.contains('input-select'))) {
+                this.valuesContainer.style = "display:none"
+                if (!this.input.value) {
+                    this.label.style = this.labelInitialStyles
+                }
+            }
+        })
+    }
 
-const inputWidth = input.offsetWidth;
-const labelInitialStyles = label.style;
+    inputAddClick() {
+        this.input.addEventListener('click', () => {
+            this.valuesContainer.style = 'display:block'
+            this.backLabelPosition()
+        })
+    }
+    valuesAddClick() {
+        this.values.forEach(element => {
+            element.addEventListener('click', () => {
+                this.input.value = element.innerText
+                this.valuesContainer.style = "display:none"
+                this.actionButton.dataset.state = "selected"
+            })
+        });
+    }
 
-itemsContainer.style.width = inputWidth - 24 + 'px'
+    actionButtonAddClick() {
+        this.actionButton.addEventListener('click', () => {
+            if (this.actionButton.dataset.state === 'ready') {
+                this.valuesContainer.style = 'display:block'
+                this.input.value = '';
+                this.controlPlaceholder()
+                this.backLabelPosition()
+                return
+            }
+            this.input.value = '';
+            this.actionButton.dataset.state = "ready"
+            this.label.style = this.labelInitialStyles;
+            this.valuesContainer.style = 'display:none'
+            this.controlPlaceholder()
+        })
+    }
 
+    initInstance() {
+        this.inputAddClick()
+        this.valuesAddClick()
+        this.actionButtonAddClick()
+        this.addClick()
+        this.backLabelPosition()
+    }
 
-const backLabelPosition = () => {
-    label.style.transform = 'translateY(-25%)';
-    label.style.top = '15%';
 }
 
-const controlPlaceholder = (show) => {
-    show ? input.placeholder = 'placeholder' : input.placeholder = ''
-}
-controlPlaceholder()
-document.body.addEventListener('click', (e) => {
-    if (!(e.target.classList.contains('action-button') || e.target.classList.contains('input-select'))) {
-        valuesContainer.style = "display:none"
-        if (!input.value) {
-            label.style = labelInitialStyles
-        }
-    }
-})
-
-
-input.addEventListener('click', () => {
-    valuesContainer.style = 'display:block'
-    backLabelPosition()
-})
-
-
-values.forEach(element => {
-    element.addEventListener('click', () => {
-        input.value = element.innerText
-        valuesContainer.style = "display:none"
-        actionButton.dataset.state = "selected"
-    })
-});
-
-actionButton.addEventListener('click', () => {
-    if (actionButton.dataset.state === 'ready') {
-        valuesContainer.style = 'display:block'
-        input.value = '';
-        controlPlaceholder()
-        backLabelPosition()
-        return
-    }
-    input.value = '';
-    actionButton.dataset.state = "ready"
-    label.style = labelInitialStyles;
-    valuesContainer.style = 'display:none'
-    controlPlaceholder()
-})
+const customSelect = new customSelectWithFloatLabel()
+customSelect.initInstance()
